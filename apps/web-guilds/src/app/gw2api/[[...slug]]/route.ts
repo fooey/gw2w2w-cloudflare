b094e3baa9d6260837c5563c362f6cc096195c15
Jason Rushton <jason@jasonrushton.com>
2026-01-27 23:25:34 -0700

    remove unused

--- apps/web-guilds/src/app/gw2api/[[...slug]]/route.ts
+++ apps/web-guilds/src/app/gw2api/[[...slug]]/route.ts
@@ -1,18 +1,14 @@
-import type { NextRequest } from 'next/server';
-
 import { getCloudflareContext } from '@opennextjs/cloudflare';
-import type { AppType as Gw2ApiAppType } from '@repo/service-gw2api';
-import { hc } from 'hono/client';
+import type { NextRequest } from 'next/server';
 
 export async function GET(
   req: NextRequest,
   ctx: RouteContext<'/gw2api/[[...slug]]'>,
 ) {
-  const { env } = process;
   const { slug } = await ctx.params;
 
   // on production this is routed to the gw2api worker directly by cloudflare
-  if (!slug || env.NODE_ENV === 'production') {
+  if (!slug || process.env.NODE_ENV === 'production') {
     return new Response('Not Found', { status: 404 });
   }
 
@@ -20,13 +16,12 @@ export async function GET(
 }
 
 async function proxyApi(slug: string, req: NextRequest) {
-  const { env } = getCloudflareContext();
   const url = `http://internal/gw2api/${slug}`;
 
   const isBodyless = req.method === 'GET' || req.method === 'HEAD';
   const body = isBodyless ? undefined : await req.arrayBuffer();
 
-  const res = await env.SERVICE_GW2API.fetch(url, {
+  const res = await getCloudflareContext().env.SERVICE_GW2API.fetch(url, {
     method: req.method,
     headers: req.headers,
     body,
