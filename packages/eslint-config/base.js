ceb27a6abd50d3880f371549ba01f85469b315ff
Jason Rushton <jason@jasonrushton.com>
2026-02-14 14:45:18 -0700

    fix: update no-console rule to disallow console.log

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import onlyWarn from 'eslint-plugin-only-warn';
 import turboPlugin from 'eslint-plugin-turbo';
 import tseslint from 'typescript-eslint';
-import onlyWarn from 'eslint-plugin-only-warn';
 
 /**
  * A shared ESLint configuration for the repository.
@@ -19,7 +19,7 @@ export const config = [
     },
     rules: {
       'turbo/no-undeclared-env-vars': 'warn',
-      'no-console': 'warn',
+      'no-console': ['warn', { disallow: ['log'] }],
     },
   },
   {
