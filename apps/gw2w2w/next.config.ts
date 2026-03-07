ef875d58f2bbb6ba4694a6b1ef05f3b29c0f1114
Jason Rushton <jason@jasonrushton.com>
2026-03-07 16:08:36 -0700

    fix: update next.config.ts to include image remote patterns and adjust imports

--- apps/gw2w2w/next.config.ts
+++ apps/gw2w2w/next.config.ts
@@ -1,11 +1,23 @@
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { getEmblemSrc } from '@gw2w2w/lib/emblems';
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 import type { NextConfig } from 'next';
 
 const faviconSrc = getEmblemSrc('97C007DC-87D5-E311-9621-AC162DAE8ACD');
+const emblemUrl = new URL(process.env.NEXT_PUBLIC_SERVICE_EMBLEM_HOST!);
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: emblemUrl.protocol.replace(':', '') as 'http' | 'https',
+        hostname: emblemUrl.hostname,
+        port: emblemUrl.port,
+        pathname: '/**',
+      },
+    ],
+  },
   redirects() {
     return [
       {
