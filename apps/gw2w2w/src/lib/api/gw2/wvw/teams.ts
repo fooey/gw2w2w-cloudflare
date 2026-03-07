5060ac2e23745bffda236d25f736c46eb56bc0c3
Jason Rushton <jason@jasonrushton.com>
2026-03-07 16:09:17 -0700

    add getWvwTeamGuildsRequest function to fetch guilds for a team

--- apps/gw2w2w/src/lib/api/gw2/wvw/teams.ts
+++ apps/gw2w2w/src/lib/api/gw2/wvw/teams.ts
@@ -3,3 +3,7 @@ import { apiFetch } from '@gw2w2w/lib/api/client';
 export function getWvwTeamRequest(teamId: string): Promise<Response> {
   return apiFetch(`/gw2/wvw/teams/team/${teamId}`);
 }
+
+export function getWvwTeamGuildsRequest(teamId: string): Promise<Response> {
+  return apiFetch(`/gw2/wvw/guilds/team/${teamId}`);
+}
