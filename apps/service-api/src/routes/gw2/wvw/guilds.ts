42c58e28cd35cc145f6eb5c88a635ffc5ac14297
Jason Rushton <jason@jasonrushton.com>
2026-03-07 16:07:15 -0700

    add region and team endpoints for WvW guilds

--- apps/service-api/src/routes/gw2/wvw/guilds.ts
+++ apps/service-api/src/routes/gw2/wvw/guilds.ts
@@ -1,8 +1,11 @@
 import { zValidator } from '@hono/zod-validator';
+import { WVW_TEAMS } from '@service-api/definitions';
 import type { CloudflareEnv, ErrorPayload } from '@service-api/index';
-import { getWvwGuild } from '@service-api/lib/resources/wvw/guilds';
+import { getWvwGuild, type WvWGuild } from '@service-api/lib/resources/wvw/guilds';
 import { Hono } from 'hono';
 import { z } from 'zod';
+
+const regions = ['na', 'eu'] as const;
 
 export const apiWvwGuildsRoute = new Hono<{ Bindings: CloudflareEnv }>()
   .get('/guild/:guildId', zValidator('param', z.object({ guildId: z.string() })), async (c) => {
@@ -20,7 +23,45 @@ export const apiWvwGuildsRoute = new Hono<{ Bindings: CloudflareEnv }>()
         return c.json(payload, 404);
       }
 
-      return c.json(wvwGuild);
+      return c.json<WvWGuild>(wvwGuild);
+    });
+  })
+  .get('/region/:region', zValidator('param', z.object({ region: z.enum(regions) })), async (c) => {
+    const region = c.req.param('region');
+
+    return getWvwGuild('all', c.env).then((wvwGuilds) => {
+      const regionGuilds = wvwGuilds?.filter((guild) => guild.region === region) || [];
+
+      if (!Array.isArray(wvwGuilds)) {
+        const payload: ErrorPayload = {
+          message: 'WvW Guilds Not Found for Region',
+          statusCode: 404,
+          url: new URL(c.req.url).pathname,
+          service: 'service-api/wvw/guilds',
+        };
+        return c.json(payload, 404);
+      }
+
+      return c.json<WvWGuild[]>(regionGuilds);
+    });
+  })
+  .get('/team/:teamId', zValidator('param', z.object({ teamId: z.enum(Object.keys(WVW_TEAMS)) })), async (c) => {
+    const teamId = c.req.param('teamId');
+
+    return getWvwGuild('all', c.env).then((wvwGuilds) => {
+      const teamGuilds = wvwGuilds?.filter((guild) => guild.teamId === teamId) || [];
+
+      if (!Array.isArray(wvwGuilds)) {
+        const payload: ErrorPayload = {
+          message: 'WvW Guilds Not Found for Team',
+          statusCode: 404,
+          url: new URL(c.req.url).pathname,
+          service: 'service-api/wvw/guilds',
+        };
+        return c.json(payload, 404);
+      }
+
+      return c.json<WvWGuild[]>(teamGuilds);
     });
   })
   .get('*', (c) => {
