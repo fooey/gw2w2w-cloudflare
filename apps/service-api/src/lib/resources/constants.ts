f98497476a37c375b0394adddd26a34a7bacb96c
Jason Rushton <jason@jasonrushton.com>
2026-03-11 17:05:57 -0600

    add STORE_STATIC_OBJECT_TTL

--- apps/service-api/src/lib/resources/constants.ts
+++ apps/service-api/src/lib/resources/constants.ts
@@ -1,6 +1,7 @@
-export const NOT_FOUND_CACHE_EXPIRATION = 3600; // 1 hour
-export const STORE_KV_TTL = 86400; // 24 hours
-export const STORE_OBJECT_TTL = 86400; // 24 hours
+export const NOT_FOUND_CACHE_EXPIRATION = 60 * 60; // 1 hour
+export const STORE_KV_TTL = 60 * 60 * 24; // 24 hours
+export const STORE_OBJECT_TTL = 60 * 60 * 24; // 24 hours
+export const STORE_STATIC_OBJECT_TTL = 60 * 60 * 24 * 365; // 1 year, objects that are not expected to change, like textures and colors
 
 export const NOT_FOUND_CACHE_VALUE = '__NOT_FOUND__';
 export function getEnableCacheLogging() {
