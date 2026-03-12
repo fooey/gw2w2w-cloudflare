bfc1e3a03157ec68b3b7d0a52c6594b50337057a
Jason Rushton <jason@jasonrushton.com>
2026-03-11 21:11:25 -0600

    add route for retrieving all colors

--- apps/service-api/src/routes/gw2/color.ts
+++ apps/service-api/src/routes/gw2/color.ts
@@ -5,32 +5,28 @@ import type { Color } from '@service-api/lib/types';
 import { Hono } from 'hono';
 import { z } from 'zod';
 
-export const apiColorRoute = new Hono<{ Bindings: CloudflareEnv }>().get(
-  '/:colorId',
-  zValidator('param', z.object({ colorId: z.coerce.number().nonnegative().max(999).nonoptional() })),
-  async (c) => {
-    const colorId = Number(c.req.param('colorId'));
+export const apiColorRoute = new Hono<{ Bindings: CloudflareEnv }>()
+  .get('/', async (c) => {
+    const colors = await getColor('all', c.env);
+    return c.json<Color[]>(colors, 200);
+  })
+  .get(
+    '/:colorId',
+    zValidator('param', z.object({ colorId: z.coerce.number().nonnegative().max(999).nonoptional() })),
+    async (c) => {
+      const colorId = Number(c.req.param('colorId'));
 
-    const color = await getColor(colorId, c.env);
-    if (!Array.isArray(color) || color.length !== 1) {
-      const payload: ErrorPayload = {
-        message: 'Color Not Found',
-        statusCode: 404,
-        url: new URL(c.req.url).pathname,
-        service: 'service-api/color',
-      };
-      return c.json(payload, 404);
-    }
+      const color = await getColor(colorId, c.env);
+      if (!Array.isArray(color) || color.length !== 1) {
+        const payload: ErrorPayload = {
+          message: 'Color Not Found',
+          statusCode: 404,
+          url: new URL(c.req.url).pathname,
+          service: 'service-api/color',
+        };
+        return c.json(payload, 404);
+      }
 
-    return c.json<Color[]>(color, 200);
-  },
-);
-// .get('*', (c) => {
-//   const payload: ErrorPayload = {
-//     message: 'Not Found',
-//     statusCode: 404,
-//     url: new URL(c.req.url).pathname,
-//     service: 'service-api/color',
-//   };
-//   return c.json(payload, payload.statusCode);
-// });
+      return c.json<Color[]>(color, 200);
+    },
+  );
