45a790ae4a14c961a6cd0e59065329e3a9ec0ab9
Jason Rushton <jason@jasonrushton.com>
2026-03-11 19:31:30 -0600

    feat: add initial DesignerPage component with layout

---
+++ apps/gw2w2w/src/app/designer/page.tsx
@@ -0,0 +1,12 @@
+import SiteLayout from '@gw2w2w/lib/ui/layout/SiteLayout';
+
+export default function DesignerPage() {
+  return (
+    <SiteLayout pageHeader={'Emblem Designer'}>
+      <header>
+        <h2 className="mb-4 text-2xl font-bold tracking-tight text-gray-900">Emblem Designer</h2>
+      </header>
+      <main>wip</main>
+    </SiteLayout>
+  );
+}
