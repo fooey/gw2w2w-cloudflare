1db0b63a9bac83dcb8a22d82d8a35c4439a72fda
Jason Rushton <jason@jasonrushton.com>
2026-03-22 15:32:04 -0600

    feat: add emblem designer integration and improve emblem handling in guild page

--- apps/gw2w2w/src/lib/emblems.ts
+++ apps/gw2w2w/src/lib/emblems.ts
@@ -1,4 +1,5 @@
 import type { EmblemState } from '@gw2w2w/lib/ui/designer/types';
+import type { Guild } from '@service-api/lib/types';
 
 const EMBLEM_HOST_PRODUCTION = 'https://emblem.gw2w2w.com';
 const EMBLEM_HOST_DEVELOPMENT = 'http://localhost:8787';
@@ -22,4 +23,18 @@ export const getCustomEmblemSrc = (emblem: EmblemState) => {
   if (emblem.flags.includes('FlipForegroundVertical')) qs.set('flags_flip_fg_vertical', '');
   const query = qs.toString();
   return `${emblemHost}/custom${query ? `?${query}` : ''}`;
+};
+
+export const getDesignerSrc = (emblem: NonNullable<Guild['emblem']>) => {
+  const qs = new URLSearchParams();
+  qs.set('background_id', String(emblem.background.id));
+  if (emblem.background.colors[0] != null) qs.set('background_color_id', String(emblem.background.colors[0]));
+  qs.set('foreground_id', String(emblem.foreground.id));
+  if (emblem.foreground.colors[0] != null) qs.set('foreground_primary_color_id', String(emblem.foreground.colors[0]));
+  if (emblem.foreground.colors[1] != null) qs.set('foreground_secondary_color_id', String(emblem.foreground.colors[1]));
+  if (emblem.flags.includes('FlipBackgroundHorizontal')) qs.set('flags_flip_bg_horizontal', '');
+  if (emblem.flags.includes('FlipBackgroundVertical')) qs.set('flags_flip_bg_vertical', '');
+  if (emblem.flags.includes('FlipForegroundHorizontal')) qs.set('flags_flip_fg_horizontal', '');
+  if (emblem.flags.includes('FlipForegroundVertical')) qs.set('flags_flip_fg_vertical', '');
+  return `/designer?${qs.toString()}`;
 };
