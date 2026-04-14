74b5ccd234117eda39699e92e394c904cac6a363
Jason Rushton <jason@jasonrushton.com>
2026-04-13 23:24:45 -0600

    prettier

--- apps/gw2w2w/src/ui/Link.tsx
+++ apps/gw2w2w/src/ui/Link.tsx
@@ -1,5 +1,8 @@
 import NextLink, { type LinkProps } from 'next/link';
 
-export default function Link({ prefetch = false, ...props }: LinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+export default function Link({
+  prefetch = false,
+  ...props
+}: LinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>) {
   return <NextLink prefetch={prefetch} {...props} />;
 }
