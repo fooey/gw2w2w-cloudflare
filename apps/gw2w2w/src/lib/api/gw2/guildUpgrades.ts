e302a8e65a6acd8e92eedd6e19941bedb26b93e5
Jason Rushton <jason@jasonrushton.com>
2026-04-19 22:12:52 -0600

    add early return for empty IDs in fetchGuildUpgrades

--- apps/gw2w2w/src/lib/api/gw2/guildUpgrades.ts
+++ apps/gw2w2w/src/lib/api/gw2/guildUpgrades.ts
@@ -3,5 +3,8 @@ import { parseResponse } from '#lib/api/utils';
 import { type GuildUpgrade } from '@repo/service-api/types';
 
 export function fetchGuildUpgrades(ids: number[]): Promise<GuildUpgrade[] | null> {
+  if (ids.length === 0) {
+    return Promise.resolve([]);
+  }
   return apiFetch(`/gw2/guild/upgrades?ids=${ids.join(',')}`).then(parseResponse<GuildUpgrade[]>);
 }
