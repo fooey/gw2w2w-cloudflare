2d05f4ca1aadd35a5ef2e8d91c92631e0c13fc24
Jason Rushton <jason@jasonrushton.com>
2026-05-20 20:44:18 -0600

    replace WvWMatchStripped with WvWMatch in various components and schema

--- apps/gw2w2w/src/ui/wvw/dashboard/MatchupRow.tsx
+++ apps/gw2w2w/src/ui/wvw/dashboard/MatchupRow.tsx
@@ -2,10 +2,10 @@
 
 import type { Lang } from '#ui/wvw/config/lang';
 import { MatchScoreboard } from '#ui/wvw/shared/MatchScoreboard';
-import type { WvWMatchStripped } from '@repo/service-api/types';
+import type { WvWMatch } from '@repo/service-api/types';
 import Link from '#ui/Link';
 
-export function MatchupRow({ match, lang }: { match: WvWMatchStripped; lang: Lang }) {
+export function MatchupRow({ match, lang }: { match: WvWMatch; lang: Lang }) {
   return (
     <li className="my-4 flex flex-row items-center">
       <div className="w-10">
