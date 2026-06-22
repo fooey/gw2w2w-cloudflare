8466efdc53bc6ed951fd09c2dcf1402abd55c310
Jason Rushton <jason@jasonrushton.com>
2026-06-22 16:12:41 -0600

    Switch Link and SiteLayout to named exports and update remaining consumers

--- apps/gw2w2w/src/ui/Link.tsx
+++ apps/gw2w2w/src/ui/Link.tsx
@@ -1,8 +1,5 @@
 import NextLink, { type LinkProps } from 'next/link';
 
-export default function Link({
-  prefetch = false,
-  ...props
-}: LinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+export function Link({ prefetch = false, ...props }: LinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>) {
   return <NextLink prefetch={prefetch} {...props} />;
 }
